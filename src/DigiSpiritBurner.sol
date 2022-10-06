2e09fb4f780ff0f241f2a45fc42385ae28b178ee
Andrew Mitchell <mitche50@gmail.com>
2022-10-05 21:36:05 -0400

    feat: add contract fees and take hero fee upfront

--- src/DigiSpiritBurner.sol
+++ src/DigiSpiritBurner.sol
@@ -5,9 +5,14 @@ import "./Adventure/DigiDaigaku.sol";
 import "./Adventure/DigiDaigakuHeroes.sol";
 import "./Adventure/DigiDaigakuSpirits.sol";
 import "./Adventure/HeroAdventure.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DigiSpiritBurner is AdventurePermissions {
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract DigiSpiritBurner is Context {
+    using SafeMath for uint256;
+
     DigiDaigaku genesisToken =
         DigiDaigaku(0xd1258DB6Ac08eB0e625B75b371C023dA478E94A9);
     DigiDaigakuHeroes heroToken =
@@ -26,14 +31,24 @@ contract DigiSpiritBurner is AdventurePermissions {
     mapping(uint16 => address) private _spiritOwner;
     mapping(uint16 => uint16) private _spiritGenesisAdventurePair;
 
+    mapping(address => uint256) public pendingRewards;
+    mapping(address => uint256) public claimableRewards;
+    uint256 private _contractClaimableRewards;
+    address contractClaimer;
+
+    uint256 public constant MAX_BPS = 10_000;
+    uint256 public constant CONTRACT_FEE_BPS = 250;
+
     event GenesisDeposited(uint16 tokenId, address owner, uint256 fee);
     event GenesisWithdrawn(uint16 tokenId, address owner);
     event GenesisFeeUpdated(uint16 tokenId, uint256 oldFee, uint256 newFee);
     event SpiritDeposited(uint16 tokenId, address owner);
     event HeroMinted(uint16 spiritId, address owner);
+    event RewardClaimed(address claimer, uint256 amount);
 
-    constructor() {
+    constructor () {
         spiritToken.setAdventuresApprovedForAll(address(adventure), true);
+        contractClaimer = _msgSender();
     }
 
     modifier onlyGenesisOwner(uint16 tokenId) {
@@ -56,6 +71,8 @@ contract DigiSpiritBurner is AdventurePermissions {
         _genesisOwner[tokenId] = _msgSender();
         heroFee[tokenId] = fee;
         genesisIsDeposited[tokenId] = true;
+        claimableRewards[_msgSender()] = 0;
+        pendingRewards[_msgSender()] = 0;
 
         emit GenesisDeposited(tokenId, _msgSender(), fee);
     }
@@ -68,7 +85,15 @@ contract DigiSpiritBurner is AdventurePermissions {
         external
         onlyGenesisOwner(tokenId)
     {
+        require(genesisToken.ownerOf(tokenId) == address(this), 'Genesis not in contract');
         genesisToken.transferFrom(address(this), _msgSender(), tokenId);
+
+        uint256 toClaim = claimableRewards[_msgSender()];
+        claimableRewards[_msgSender()] = 0;
+        if(toClaim > 0) {
+            weth.transferFrom(address(this), _msgSender(), toClaim);
+        }
+
         _genesisOwner[tokenId] = address(0);
         heroFee[tokenId] = 0;
         genesisIsDeposited[tokenId] = false;
@@ -85,7 +110,7 @@ contract DigiSpiritBurner is AdventurePermissions {
         external
         onlyGenesisOwner(tokenId)
     {
-        require(genesisToken.ownerOf(tokenId) == address(this), "Genesis is on an adventure - must be home to update fee");
+        require(genesisToken.ownerOf(tokenId) == address(this), "Genesis must be home to update fee");
         uint256 oldFee = heroFee[tokenId];
         heroFee[tokenId] = newFee;
 
@@ -111,11 +136,14 @@ contract DigiSpiritBurner is AdventurePermissions {
         external
         onlySpiritOwner(tokenId)
     {
+        require(_spiritGenesisAdventurePair[tokenId] == 0, 'Mint or quit quest to withdraw');
         spiritToken.transferFrom(address(this), _msgSender(), tokenId);
     }
 
     /**
      * @notice Enter the hero quest with the provided spirit and genesis token pair
+     * @notice Payment is required up front for the hero token, with half being refunded
+     * @notice if you decide to cancel before the quest is finished.
      * @param spiritId ID of the spirit token to be burnt for a hero
      * @param genesisId ID of the genesis token to be used
      */
@@ -123,6 +151,12 @@ contract DigiSpiritBurner is AdventurePermissions {
         external
         onlySpiritOwner(spiritId)
     {
+        // Transfer wETH to the contract
+        weth.transferFrom(_msgSender(), address(this), heroFee[genesisId]);
+        
+        // Assign pending rewards to genesis 
+        pendingRewards[_genesisOwner[genesisId]] += heroFee[genesisId];
+
         genesisToken.approve(address(adventure), genesisId);
         adventure.enterQuest(spiritId, genesisId);
         _spiritGenesisAdventurePair[spiritId] = genesisId;
@@ -132,34 +166,102 @@ contract DigiSpiritBurner is AdventurePermissions {
      * @notice Remove spirit from quest and return to user without minting hero
      * @notice allows for a user to remove their spirit before the duration of the quest is completed
      * @notice but costs half of the heroFee assigned to the genesis token to prevent griefing
-     * @dev Requires the user to have approved the contract to spend wETH fee
      * @param spiritId ID of the spirit token to be burnt for a hero
      */
     function rageQuitHeroQuest(uint16 spiritId)
         external
         onlySpiritOwner(spiritId)
     {
-        // Transfer wETH Fee to Genesis Holder (halved due to not getting hero)
+        // Assign claimable rewards to Genesis Holder (halved due to not getting hero)
         uint16 genesisId = _spiritGenesisAdventurePair[spiritId];
-        weth.transferFrom(_msgSender(), _genesisOwner[genesisId], heroFee[genesisId] / 2);
+        require(genesisId > 0, 'Spirit is not on a hero quest');
+
+        address genesisHolder = _genesisOwner[genesisId];
+        uint256 fee = heroFee[genesisId] / 2;
+        uint256 contractFee = _calculateFee(fee, CONTRACT_FEE_BPS);
+
+        pendingRewards[genesisHolder] -= fee;
+        claimableRewards[genesisHolder] += fee - contractFee;
+        _contractClaimableRewards += contractFee;
+
         adventure.exitQuest(spiritId, false);
+        delete _spiritGenesisAdventurePair[spiritId];
+
+        // Return spirit token and remaining wETH to owner
         spiritToken.transferFrom(address(this), _msgSender(), spiritId);
+        weth.transferFrom(address(this), _msgSender(), heroFee[genesisId] - fee);
     }
 
     /**
      * @notice Mint a hero from the provided token ID.
+     * @notice Anyone can perform this action as the fee has already been collected from the spirit owner
      * @dev Requires the user to have waited the `HERO_QUEST_DURATION` from the HeroAdventure contract
-     * @dev and the user to have approved the contract to spend wETH fee
      * @param spiritId ID of the spirit token to be burnt for a hero
      */
-    function mintHero(uint16 spiritId) external onlySpiritOwner(spiritId) {
+    function mintHero(uint16 spiritId) external {
         // Transfer wETH Fee to Genesis Holder
         uint16 genesisId = _spiritGenesisAdventurePair[spiritId];
-        weth.transferFrom(_msgSender(), _genesisOwner[genesisId], heroFee[genesisId]);
+        require(genesisId > 0, 'Spirit is not on a hero quest');
+        address genesisHolder = _genesisOwner[genesisId];
+
+        uint256 fee = heroFee[genesisId];
+        uint256 contractFee = _calculateFee(fee, CONTRACT_FEE_BPS);
+        
+        pendingRewards[genesisHolder] -= fee;
+        claimableRewards[genesisHolder] += fee - contractFee;
+        _contractClaimableRewards += contractFee;
 
         adventure.exitQuest(spiritId, true);
-        heroToken.transferFrom(address(this), _msgSender(), spiritId);
+        heroToken.transferFrom(address(this), _spiritOwner[spiritId], spiritId);
+        delete _spiritGenesisAdventurePair[spiritId];
 
-        emit HeroMinted(spiritId, _msgSender());
+        emit HeroMinted(spiritId, _spiritOwner[spiritId]);
     }
+
+    /**
+     * @notice Claim the rewards allocated to the caller's address
+     */
+    function claimRewards() external {
+        require(claimableRewards[_msgSender()] > 0, 'No claimable rewards');
+        uint256 toClaim = claimableRewards[_msgSender()];
+        delete claimableRewards[_msgSender()];
+        weth.transferFrom(address(this), _msgSender(), toClaim);
+
+        emit RewardClaimed(_msgSender(), toClaim);
+    }
+
+    /**
+     * @notice Claim the rewards allocated to the contract
+     */
+     function claimContractRewards() external {
+        require(_msgSender() == contractClaimer, 'Only contract claimer');
+        uint256 toClaim = _contractClaimableRewards;
+        _contractClaimableRewards = 0;
+        weth.transferFrom(address(this), contractClaimer, toClaim);
+
+        emit RewardClaimed(_msgSender(), toClaim);
+     }
+
+     /**
+     * @notice Update the contract claimer
+     */
+     function updateContractClaimer(address newClaimer) external {
+        require(_msgSender() == contractClaimer, 'Only contract claimer');
+        contractClaimer = newClaimer;
+     }
+
+    /**
+     * @dev Helper function to calculate fees.
+     * @param amount Amount to calculate fee on.
+     * @param feeBps The fee to be charged in basis points.
+     * @return Amount of fees to take.
+     */
+    function _calculateFee(uint256 amount, uint256 feeBps) internal pure returns (uint256) {
+        if (feeBps == 0) {
+            return 0;
+        }
+        uint256 fee = amount.mul(feeBps).div(MAX_BPS);
+        return fee;
+    }
+
 }
